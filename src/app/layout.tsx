94d03d0b2c9d23e9ffcc8c23cef9b862f1f0eef6
shak33 <maciej@poppek.pl>
2024-07-02 19:00:37 +0200

    [OPS-10] Installed tanstack/react-query, axios, react-hook-form, react-icons, zod, zustand, created Navbar and Modal components, installed shadcn-ui button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+
+import { Navbar } from '@/components/Navbar/Navbar.comp';
 
 export const metadata: Metadata = {
-  title: "Open School",
-  description: "Manage your school(s) with ease",
+  title: 'Open School',
+  description: 'Manage your school(s) with ease',
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar/Navbar.comp.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center p-4 bg-white text-black shadow-md">
+      <div className="flex items-center space-x-4">
+        <div className="text-2xl font-bold">Open School</div>
+        <div className="flex items-center space-x-4">
+          <Button variant="link" className="text-base hover:underline">
+            <Link href="#" className="hover:underline">
+              Home
+            </Link>
+          </Button>
+          <Button variant="link" className="text-base hover:underline">
+            <Link href="#" className="hover:underline">
+              About
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Button variant="link" className="text-base hover:underline">
+          <Link href="#">Login</Link>
+        </Button>
+        <Button variant="link" className="text-base hover:underline">
+          <Link href="#">Register</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
