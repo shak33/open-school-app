bd61bbf2518f447ea29b5d95710a899cb3317e57
shak33 <maciej@poppek.pl>
2024-07-20 15:06:57 +0200

    [OPS-18] Removed old radix select from CustomTableFooter and implemented react-select version (CustomSelect)

---
+++ src/components/CustomTable/components/CustomTableFooter/models/CustomTableFooterRowsPerPage.model.ts
@@ -0,0 +1,18 @@
+export const CustomTableFooterRowsPerPage = [
+  {
+    value: '10',
+    label: '10',
+  },
+  {
+    value: '25',
+    label: '25',
+  },
+  {
+    value: '50',
+    label: '50',
+  },
+  {
+    value: '100',
+    label: '100',
+  },
+];
--- src/components/form/CustomSelect/CustomSelect.comp.tsx
+++ src/components/form/CustomSelect/CustomSelect.comp.tsx
@@ -6,17 +6,22 @@ interface Props {
   name: string;
   options: CustomSelectOption<string>[];
   value: CustomSelectOption;
+  onChange?: () => void;
 }
 
 type SelectedValue = SingleValue<CustomSelectOption<string>>;
 
-export const CustomSelect = ({ name, options, value }: Props) => {
+export const CustomSelect = ({ name, options, value, onChange }: Props) => {
   const formMethods = useFormContext();
 
   formMethods.register(name);
 
   const onCustomSelectChange = (selectedValue: SelectedValue) => {
     formMethods.setValue(name, selectedValue);
+
+    if (typeof onChange === 'function') {
+      onChange();
+    }
   };
 
   return (
